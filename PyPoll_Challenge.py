540128b4e7a6cadf04c0466c124566c8b6ec5502
Kossakova <zkossakova@gmail.com>
2021-09-09 21:16:08 -0500

    Adding final changes

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -104,7 +104,7 @@ with open(file_to_save, "w") as txt_file:
 
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
-
+        
          # 6f: Write an if statement to determine the winning county and get its vote count.
         if votes > winning_vote:
             winning_vote = votes
@@ -120,7 +120,7 @@ with open(file_to_save, "w") as txt_file:
 
     # 8: Save the county with the largest turnout to a text file.
     txt_file.write(winning_county_summary)
-
+    winning_percentage = 0 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
 
@@ -135,7 +135,7 @@ with open(file_to_save, "w") as txt_file:
         print(candidate_results)
         #  Save the candidate results to our text file.
         txt_file.write(candidate_results)
-
+        
         # Determine winning vote count, winning percentage, and candidate.
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
